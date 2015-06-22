2d0ce66527292ec96e9322528f5e678c67bd34d8
Michael Silvestre <willtard@gmail.com>
2015-06-22 15:25:26 +0200

    improve design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shifterbelt GPS Tracking Demo' });
+  res.render('index', { title: 'Shifterbelt Realtime GPS Tracking Demo' });
 });
 
 module.exports = router;
